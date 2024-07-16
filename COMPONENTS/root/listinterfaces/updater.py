d5c29a89a7ef201c07664a16119aca304a62e796
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-07-17 00:44:19 +0100

    updater was still using FO.

--- COMPONENTS/root/listinterfaces/updater.py
+++ COMPONENTS/root/listinterfaces/updater.py
@@ -3,6 +3,10 @@ from COMPONENTS.root.componentupdater import found_new_interface
 from COMPONENTS.network.componentupdater import found_our_ip_for_a_network
 from COMPONENTS.interface.componentupdater import found_new_network_for_interface
 
+from COMPONENTS.filteredobjects.filteredfoundnewnetworkforinterface import Filtered_FoundNewNetworkForInterface
+from COMPONENTS.filteredobjects.filteredfoundouripfornetwork import Filtered_FoundOurIPForNetwork
+from COMPONENTS.filteredobjects.filteredfoundnewinterface import Filtered_FoundNewInterface
+
 def update_list_interfaces(context:dict, filtered_objects:list):
 	"""
 	Defines the components we update when we list the interfaces available
@@ -11,19 +15,19 @@ def update_list_interfaces(context:dict, filtered_objects:list):
 
 	for filtered_obj in filtered_objects:
 		# FOUND NEW INTERFACE
-		if isinstance(filtered_obj, FO.Filtered_NewInterface):
+		if isinstance(filtered_obj, Filtered_FoundNewInterface):
 			interface_name = filtered_obj.get_interface_name()
 			found_new_interface(interface_name)
 			
 
 		# FOUND NEW NETWORK FOR AN INTERFACE
-		elif isinstance(filtered_obj, FO.Filtered_NewNetworkForInterface):
+		elif isinstance(filtered_obj, Filtered_FoundNewNetworkForInterface):
 			interface_name = filtered_obj.get_interface_name()
 			network_address = filtered_obj.get_network_address()
 			found_new_network_for_interface(interface_name, network_address)
 
 		# FOUND OUR IP
-		elif isinstance(filtered_obj, FO.Filtered_FoundOurIPForNetwork):
+		elif isinstance(filtered_obj, Filtered_FoundOurIPForNetwork):
 			interface_name = filtered_obj.get_interface_name()
 			network_address = filtered_obj.get_network_address()
 			ip = filtered_obj.get_ip()
