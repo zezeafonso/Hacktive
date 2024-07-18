c4a664a90646dfc0dcf2b50d4727027d56bfaac1
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-07-18 02:03:49 +0100

    added prints

--- COMPONENTS/domains/enumdomainusersthroughrpc/updater.py
+++ COMPONENTS/domains/enumdomainusersthroughrpc/updater.py
@@ -11,15 +11,17 @@ def update_enum_domain_users_through_rpc(context:dict, filtered_objects:list):
 		return 
 
 	with sharedvariables.shared_lock:
-		domain = sharedvariables.root_obj.get_or_create_domain(context['domain_name'])
+		domain_name = context['domain_name']
+		print(domain_name)
+		domain = sharedvariables.root_obj.get_or_create_domain(domain_name)
   
-	for filtered_obj in filtered_objects:
-		# found domain user
-		if isinstance(filtered_obj, Filtered_FoundDomainUserThroughRPC):
-			logger.debug(f"filter for enum domain users through rpc found user ({filtered_obj.get_user()}) with rid ({filtered_obj.get_rid()})")
+		for filtered_obj in filtered_objects:
+			# found domain user
+			if isinstance(filtered_obj, Filtered_FoundDomainUserThroughRPC):
+				logger.debug(f"filter for enum domain users through rpc found user ({filtered_obj.get_user()}) with rid ({filtered_obj.get_rid()})")
 
-			username = filtered_obj.get_user()
-			rid = filtered_obj.get_rid()
-			found_user_for_domain(domain, username, rid)
+				username = filtered_obj.get_user()
+				rid = filtered_obj.get_rid()
+				found_user_for_domain(domain, username, rid)
 
 	return 
