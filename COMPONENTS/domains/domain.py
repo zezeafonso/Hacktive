874962fd68e25c219f86372f9e57a8b494629eb1
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-07-19 15:50:20 +0100

    added the method of enuumerating the groups to the list of methods in the domain component

--- COMPONENTS/domains/domain.py
+++ COMPONENTS/domains/domain.py
@@ -12,6 +12,7 @@ from THREADS.runcommandsthread import send_run_event_to_run_commands_thread
 
 from COMPONENTS.domains.enumdomainsthroughrpc.method import EnumDomainsThroughRPC
 from COMPONENTS.domains.enumdomainusersthroughrpc.method import EnumDomainUsersThroughRPC
+from COMPONENTS.domains.enumdomaingroupsthroughrpc.method import EnumDomainGroupsThroughRPC
 
 
 class Domain(AbstractNetworkComponent):
@@ -25,7 +26,7 @@ class Domain(AbstractNetworkComponent):
 	change the methods to receive the context like this
  	methods = [EnumDomainsThroughRPC, EnumDomainTrustsThroughRPC, EnumDomainUsersThroughRPC, EnumDomainGroupsThroughRPC]
   	"""
-	methods = [EnumDomainsThroughRPC, EnumDomainUsersThroughRPC]
+	methods = [EnumDomainsThroughRPC, EnumDomainUsersThroughRPC, EnumDomainGroupsThroughRPC]
 	roles = ["DC", "machine"]
 
 	def __init__(self, domain_name:str, domain_pdc:'LdapServer'=None):
