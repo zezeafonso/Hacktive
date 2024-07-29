9702c8d2ca71e381494acd85e3951da8522aab2c
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-07-29 18:05:37 +0100

    filter must be a class not a function. Solved

--- COMPONENTS/domains/listsharesthroughsmb/filter.py
+++ COMPONENTS/domains/listsharesthroughsmb/filter.py
@@ -2,6 +2,10 @@ import re
 
 from COMPONENTS.abstract.abstractfilter import AbstractFilter
 
-def ListSharesThroughSMB_Filter(AbstractFilter):
-    # nothing for now
-    return
+
+class ListSharesThroughSMB_Filter(AbstractFilter):
+	_name = "filter list sharers through SMB"
+
+	@staticmethod
+	def filter(output:str) -> list:
+		return []
