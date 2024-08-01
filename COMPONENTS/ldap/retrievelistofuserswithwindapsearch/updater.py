cf56d24672c7602f4a624ebbcd61ece9292fbc6d
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-08-02 00:52:00 +0100

    nothing actually updated, see if changes

--- COMPONENTS/ldap/retrievelistofuserswithwindapsearch/updater.py
+++ COMPONENTS/ldap/retrievelistofuserswithwindapsearch/updater.py
@@ -17,14 +17,12 @@ def retrieve_list_users_with_windapsearch_updater(context:dict, filtered_objects
 		domain = sharedvariables.root_obj.get_or_create_domain(domain_name)
 
 		for fo in filtered_objects:
-			"""
 			if isinstance(fo, Filtered_FoundDistinguishedNameForDomainUser):
 				username = fo.get_sam_account_name()
 				distinguished_name = fo.get_distinguished_name()	
 				logger.debug(f"Filter foud distinguished name ({distinguished_name})\
         for user ({username})")
-				found_distinguished_name_for_sam_account_name(domain, username, distinguished_name)
-			"""
+				#found_distinguished_name_for_sam_account_name(domain, username, distinguished_name)
 
 			
 			if isinstance(fo, Filtered_FoundUserPrincipalNameForDomainUser):
@@ -32,6 +30,6 @@ def retrieve_list_users_with_windapsearch_updater(context:dict, filtered_objects
 				user_principal_name = fo.get_user_principal_name()
 				logger.debug(f"Filter found user principal name ({user_principal_name})\
         for user ({username})")
-				found_user_principal_name_for_sam_account_name(domain, username, user_principal_name)
+				#found_user_principal_name_for_sam_account_name(domain, username, user_principal_name)
 			
 	return 
