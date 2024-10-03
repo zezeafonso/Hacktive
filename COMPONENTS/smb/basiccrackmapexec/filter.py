1353d272ffdb6f5b852307f92a5763ba4d6bf50d
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-10-03 16:41:35 +0100

    updated filter to basic crackmapexec

--- COMPONENTS/smb/basiccrackmapexec/filter.py
+++ COMPONENTS/smb/basiccrackmapexec/filter.py
@@ -8,6 +8,7 @@ from COMPONENTS.filteredobjects.filteredfoundsmbservicesigning import Filtered_F
 from COMPONENTS.filteredobjects.filteredfoundsmbv1value import Filtered_FoundSMBv1Value
 from COMPONENTS.filteredobjects.filteredfounddomainofmachine import Filtered_FoundDomainOfMachine
 from COMPONENTS.filteredobjects.filteredfoundsmbserverversion import Filtered_FoundSMBServerVersion
+from COMPONENTS.filteredobjects.filteredfoundnetbioshostname import Filtered_FoundNetBIOSHostname
 
 class BasicCrackMapExec_Filter(AbstractFilter):
 	_name = "filter basic crackmapexec"
@@ -23,13 +24,14 @@ class BasicCrackMapExec_Filter(AbstractFilter):
 
 		# Extract server version
 		server_version = version_pattern.group(1).strip() if version_pattern else None
-
+		list_fo.append(Filtered_FoundSMBServerVersion(server_version))
+  
 		# Extract valuable fields
 		fields = {field[0].strip(): field[1].strip() for field in fields_pattern}
 		for field in fields: 
 			# hostname
 			if field == 'name':
-				list_fo.append(Filtered_FoundSMBServerVersion(fields[field]))
+				list_fo.append(Filtered_FoundNetBIOSHostname(fields[field]))
 			if field == 'domain':
 				list_fo.append(Filtered_FoundDomainOfMachine(fields[field]))
 			if field == 'signing':
