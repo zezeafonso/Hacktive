2c4f263ca382a48c0e1713fb1e5a68bc977480cf
zezeafonso <jose.pinto.afonso@tecnico.ulisboa.pt>
2024-10-03 17:26:31 +0100

    I had changed the wrong query and now we weren't getting the list of users

--- COMPONENTS/domains/enumdomainusersthroughrpc/filter.py
+++ COMPONENTS/domains/enumdomainusersthroughrpc/filter.py
@@ -2,36 +2,25 @@ import re
 
 from COMPONENTS.abstract.abstractfilter import AbstractFilter
 from COMPONENTS.filteredobjects.filteredfounddomainuserthroughrpc import Filtered_FoundDomainUserThroughRPC
-from COMPONENTS.filteredobjects.filteredfoundusernamefromqueryuser import Filtered_FoundUsernameFromQuery
-from COMPONENTS.filteredobjects.filteredfounddescriptionofdomainuser import Filtered_FoundDescriptionOfDomainUser
-from COMPONENTS.filteredobjects.filteredfounddomainuserridthroughrpc import Filtered_FoundDomainUserRidThroughRPC
 
 class EnumDomUsersThroughRPC_Filter(AbstractFilter):
 	_name = "filter enum domain users through rpc"
 
 	@staticmethod
 	def filter(output:str) -> list:
-		# Define a regex pattern to capture sharename, type, and comment from each line
-		username_pattern = re.compile(r"User Name\s*:\s*(\S+)")
-		description_pattern = re.compile(r"Description\s*:\s*(.*)")
-		user_rid_pattern = re.compile(r"user_rid\s*:\s*(\S+)")
-		
-		# the list of filtered objects
-		list_fo = list()
-		
-		# Split the output by lines and iterate through each line
+		findings = list()
+		# Define a regular expression pattern
+		pattern = re.compile(r"user:\[([^\]]+)\] rid:\[([^\]]+)\]")
+
+		# Iterate over each line and search for matches
 		for line in output.splitlines():
-			# Try to match the line against the pattern
-			username_match = username_pattern.match(line)
-			description_match = description_pattern.match(line)
-			user_rid_match = user_rid_pattern.match(line)
-			if username_match:
-				username = username_match.group(1)
-				list_fo.append(Filtered_FoundUsernameFromQuery(username))
-			elif description_match:
-				description = description_match.group(1)
-				list_fo.append(Filtered_FoundDescriptionOfDomainUser(description))
-			elif user_rid_match: 
-				user_rid = user_rid_match.group(1)	
-				list_fo.append(Filtered_FoundDomainUserRidThroughRPC(user_rid))
-		return list_fo
+			match = pattern.search(line)
+			if match:
+				user = match.group(1)
+				rid_hex = match.group(2)
+				rid_hex_str = str(rid_hex)
+				rid_dec = int(rid_hex, 16)
+				rid_str = str(rid_dec)
+
+				findings.append(Filtered_FoundDomainUserThroughRPC(user, rid_hex_str))
+		return findings
